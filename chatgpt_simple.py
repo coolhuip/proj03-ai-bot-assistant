100cb1cc0c1adc026813bc0a69ae5e9a0b559997
Danny <cool.huip@gmail.com>
2023-07-02 21:09:28 -0400

    Routine

--- chatgpt_simple.py
+++ chatgpt_simple.py
@@ -1,14 +1,17 @@
-import os
-import openai
 from dotenv import load_dotenv
+import openai, os
 
 load_dotenv()
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+# Let's connect our Python script to an OpenAI account.
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Now, let's ask the "gpt-3.5-turbo" model to give us a reply to the prompt:
+# < messages[0]["content"] >
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Give me your best dad joke!"}]
+    messages=[{"role": "user", "content": "Who are your Top 5 rappers of all time?"}]
 )
 print(completion.choices[0].message.content)
 
+yo = {"what": "up", "peep":"hle"}
