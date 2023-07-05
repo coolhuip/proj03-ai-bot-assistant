d67203a9af770c2938688accdc7c01645adbb965
Danny <cool.huip@gmail.com>
2023-07-05 00:38:22 -0400

    Clean up code

--- chatgpt_chat_assistant.py
+++ chatgpt_chat_assistant.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv; load_dotenv()
-import openai, os; openai.api_key = os.getenv("OPENAI_API_KEY")
+import openai, os; openai.api_key = os.getenv('OPENAI_API_KEY')
 
 NL = '\n'
 
 
 messages = []
-system_msg = input("What type of chatbot would you like to create?\n")
+system_msg = input('What type of chatbot would you like to create?\n')
 messages.append(
-    {"role": "system",
-     "content": system_msg
+    {'role': 'system',
+     'content': system_msg
     }
 )
 print("Your new assistant is ready!")
@@ -17,18 +17,18 @@ print("Your new assistant is ready!")
 while input != "quit()":
     msg = input()
     messages.append(
-        {"role": "user",
-         "content": msg
+        {'role': "user",
+         'content': msg
         }
     )
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
+        model = 'gpt-3.5-turbo',
         messages = messages
     )
     reply = response["choices"][0]["message"]["content"]
     messages.append(
-        {"role": "assistant",
-         "content": reply
+        {'role': "assistant",
+         'content': reply
         }
     )
     print(NL + reply + NL)
